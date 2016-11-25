7805c2941d0c928ffd547daad9a7dac4b32083ab
DevDorian <dorianlazaro@gmail.com>
2016-11-25 18:21:22 +0000

    updated nav

--- public/index.php
+++ public/index.php
@@ -18,26 +18,27 @@
 
 <body>
     <!--Nav Bar-->
-    <nav class="navbar navbar-inverse navbar-fixed-top " data-spy="" data-offset-top="">
+    <nav class="navbar navbar-inverse navbar-fixed-top ">
 	  	<div class="container-fluid">
 	    	<div class="navbar-header">
-	    	 	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+	    	 	<button type="button" class="navbar-toggle">
 			        <span class="icon-bar"></span>
 			        <span class="icon-bar"></span>
 			        <span class="icon-bar"></span> 
       			</button>
-	      		<a class="navbar-brand" href="index.html">Dorian Dominguez</a>
+	      		<a class="navbar-brand" href="index.html">The Handy Gringo</a>
 	    	</div>
 			<div class="collapse navbar-collapse" id="navbar" >
 				<ul class="nav navbar-nav">
-					<li><a href="#aboutSection">About Me</a></li> 
-		        	<li><a href="#portfolioSection">Portfolio</a></li>
-		        	<li><a href="#contactSection">Contact</a></li> 
+					<li><a href="#aboutSection">The Gringo's Promise</a></li> 
+		        	<li><a href="#portfolioSection">About The Gringo</a></li>
+		        	<li><a href="#contactSection">Contact Me</a></li> 
 	    		</ul>
     		</div>
 	    </div>
 	</nav>
     <!--Nav Bar End-->
+    
     <div class="jumbotron text-center">
         <h1>Welcome to The Handy Gringo</h1>
     </div>
