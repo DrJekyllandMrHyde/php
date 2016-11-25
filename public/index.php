5117cdfa0beefd83b5d1390dc7d4dd432649023b
DevDorian <dorianlazaro@gmail.com>
2016-11-25 17:36:49 +0000

    Added bootstrap and jQuery files

--- public/index.php
+++ public/index.php
@@ -1,1 +1,34 @@
-<?php echo "<h1>Welcome to the Handy Gringo</h1>"; ?>
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>The Handy Gringo</title>
+    <!-- Link to css styles -->
+    <link href="/css/styles.css" rel="stylesheet"> 
+    <!--Link to Bootstrap-->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    
+    
+</head>
+
+
+<body>
+    <div class="jumbotron">
+        <h1>Welcome to The Handy Gringo</h1>
+    </div>
+    
+    
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!--Link to jQuery-->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+
+</html>
+
