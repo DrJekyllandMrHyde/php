5f3d56ea81119870fab5b4052bf49cd992c70be1
DevDorian <dorianlazaro@gmail.com>
2016-11-27 16:18:45 +0000

    Added color to news well

--- public/includes/newsletter.php
+++ public/includes/newsletter.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<div class="row">
         <div class="span12">
-    		<div id="newsletter" class="thumbnail center well well-small text-center">
+    		<div class="thumbnail center well well-small text-center" style="color:#818C38;">
                 <h2>Newsletter</h2>
                 
                 <p>Enter your email to sign up for the Gringo Gazette!</p>
