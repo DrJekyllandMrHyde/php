46e8fb268d34d260b97f89e9de3667f0689102d3
DevDorian <dorianlazaro@gmail.com>
2016-11-28 17:04:58 +0000

    Added lorem ipsom to about

--- public/promise.php
+++ public/promise.php
@@ -9,7 +9,18 @@
 include("includes/header.php"); 
 
 ?>
+    <div class="container-fluid text-center" style="margin-top: 100px;">
+        <h1>The Gringo's Promise</h1>
+        <hr>
+        <h3>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</h3>
+    </div>
+    
+    
+<?php 
 
+    define("TITLE", "Promise | The Handy Gringo");
+    
+?>
 
 
 <?php
