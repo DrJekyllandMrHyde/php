7c9479c1a8c25a42c4c1e3d81b7b15dce380a695
DevDorian <dorianlazaro@gmail.com>
2016-12-07 07:09:49 +0000

    Fixed font size issue

--- public/promise.php
+++ public/promise.php
@@ -14,16 +14,16 @@ include("includes/header.php");
         <div class="row">
             <div class="col-sm-12 col-md-12">
                 
-                <h1 style="font-size:900%">The Gringo's Promise</h1>
+                <h1 style="font-size:550%">The Gringo's Promise</h1>
                 <hr>
                 <br>
-                <h2 style="font-size:300%"><u>I promise to show up on time and ready to work.</u></h2>
+                <h2 style="font-size:200%"><u>I promise to show up on time and ready to work.</u></h2>
                 <br>
-                <h2 style="font-size:300%"><u>I promise to give you an accurate estimate.</u> </h2>
+                <h2 style="font-size:200%"><u>I promise to give you an accurate estimate.</u> </h2>
                 <br>
-                <h2 style="font-size:300%"><u>I promise you won't pay until you're happy with the job.</u> </h2>
+                <h2 style="font-size:200%"><u>I promise you won't pay until you're happy with the job.</u> </h2>
                 <br>
-                <h2 style="font-size:300%"><u>I promise you'll be so happy you'll call me back the next time you need me.</u></h2>
+                <h2 style="font-size:200%"><u>I promise you'll be so happy you'll call me back the next time you need me.</u></h2>
                 
                 
             </div>
