38cd16adfa18719e76e0f3a07c6a977b3cc63ceb
DevDorian <dorianlazaro@gmail.com>
2016-12-14 22:53:24 +0000

    Added sendgrid and composer file

--- public/contact.php
+++ public/contact.php
@@ -34,40 +34,10 @@
 			<span class="contactHead"><h2>Drop Me A Line!</h2></span>
 			<br>
 			
-<?php
-	
-	$name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $from = 'From:'; 
-    $to = 'dorianlazaro@gmail.com'; 
-    $subject = 'Website Message';
-    $human = $_POST['human'];
-			
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-    
-		if ($_POST['submit']) {
-		if ($name != '' && $email != '') {
-		if ($human == '4') {				 
-		if (mail ($to, $subject, $body, $from)) { 
-			echo '<p>Your message has been sent!</p>';
-			} else { 
-			echo '<p>Something went wrong, go back and try again!</p>'; 
-			} 
-			
-			} else if ($_POST['submit'] && $human != '4') {
-			echo '<p>You answered the anti-spam question incorrectly!</p>';
-			}
-			
-			} else {
-			echo '<p>You need to fill in all required fields!!</p>';
-		}
-	}
 
-?>
 			
 			<!--Start of Form-->
-			<form action="contact.php" method="POST">
+			<form action="mail.php" method="POST">
 				<!-- Name Input -->
 				<div class="form-group">
   				    <label for="usr">Name:</label>
